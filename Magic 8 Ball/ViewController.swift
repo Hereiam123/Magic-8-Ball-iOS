f792a864f5431d841bfa0946abe5166abec619d8
user139727 <user139727@74-80-245-36.ic2net.net>
2018-07-07 12:06:17 -0700

    Added random image generator in View Controller

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let ballArray = ["ball1", "ball2","ball3","ball4","ball5"]
+    
+    var randomBallNumber : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        randomBallNumber = Int(arc4random_uniform(4))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+    }
+    
 }
 
